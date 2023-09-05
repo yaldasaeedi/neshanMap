b1a9366faff7df05b5b6371eeae1728c6ca1d5fa
yaldasaeedi <yalda.saeedi1381@gmail.com>
2023-09-05 12:24:43 +0330

    Initial Commit

---
+++ neshanMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neshanMap
+//
+//  Created by Helen Besharatian on 6/14/1402 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ neshanMapUITests/neshanMapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  neshanMapUITestsLaunchTests.swift
+//  neshanMapUITests
+//
+//  Created by Helen Besharatian on 6/14/1402 AP.
+//
+
+import XCTest
+
+final class neshanMapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
